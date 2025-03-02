7971aab813c030b0ce5cdf2f493d259a7caa9d96
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-02 19:55:10 +0530

    add employee  model

---
+++ models/employee.model.js
@@ -0,0 +1,19 @@
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./user');
+const Department = require('./department');
+const Designation = require('./designation');
+
+const Employee = sequelize.define('Employee', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    userId: { type: DataTypes.INTEGER, unique: true, allowNull: false, references: { model: User, key: 'id' } },
+    departmentId: { type: DataTypes.INTEGER, references: { model: Department, key: 'id' } },
+    designationId: { type: DataTypes.INTEGER, references: { model: Designation, key: 'id' } },
+    dateOfJoining: { type: DataTypes.DATE },
+    salary: { type: DataTypes.DECIMAL(10,2) }
+}, {
+    timestamps: true
+});
+
+module.exports = Employee;
