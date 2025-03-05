e244786033631ab7f04ff0e54837496d2766082f
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-05 10:08:37 +0530

    added user router and controller

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const {sequelize} = require('../config/db.js');
 
 const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
